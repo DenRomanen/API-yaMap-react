3b9e5fa8cfe1c074ac1fbe9e4d31c7b74d74531a
Denis Romanenko <romadenn@gmail.com>
2019-11-19 00:24:23 +0300

    Updates

---
+++ precache-manifest.a897302a7641662bf5c57705e940c75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef51cc4edd279267b0600383650f475f",
+    "url": "/API-yaMap-react/./index.html"
+  },
+  {
+    "revision": "f984ee0c09572bbf28ed",
+    "url": "/API-yaMap-react/./static/css/main.503238b9.chunk.css"
+  },
+  {
+    "revision": "2f1efcf9f6f2aa646c09",
+    "url": "/API-yaMap-react/./static/js/2.32f7485e.chunk.js"
+  },
+  {
+    "revision": "f984ee0c09572bbf28ed",
+    "url": "/API-yaMap-react/./static/js/main.eefaccca.chunk.js"
+  },
+  {
+    "revision": "ac839b3b1dfc83fb860f",
+    "url": "/API-yaMap-react/./static/js/runtime-main.36ecf209.js"
+  }
+]);
